a09fb4ef6c8c56539997cc96d9f03cd9460d116a
Corvax <vlvete1990@yandex.ru>
2022-02-20 12:50:19 +0300

    chore: mind grenade

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 // Modules = Encapsulated Code (only shary minimum)
 const { john, peter } = require('./4-names');
 const sayHi = require('./5-utils');
-
-sayHi('susan');
-sayHi(john);
-sayHi(peter);
+require('./7-mind-grenade');
+// sayHi('susan');
+// sayHi(john);
+// sayHi(peter);
   
