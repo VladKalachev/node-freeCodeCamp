85081c259db25b03ef54f7ac18ca5e00bac8dfc8
Corvax <vlvete1990@yandex.ru>
2022-02-20 09:59:17 +0300

    chore: text

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log('Node!');
+
+// __dirname - path ti cyrrent directory
+// __filename = file name
+// require - function to use modules (CommonJS)
+// module - info about current module (file)
+// process - info about env where the program is being executed
